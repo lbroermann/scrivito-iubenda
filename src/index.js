292e5798528f8d18f6764b1f611062f805cad2fe
lbroermann <kontakt@lennartbroermann.de>
2020-04-02 19:16:23 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import * as Scrivito from 'scrivito';
+import axios from 'axios';
+
+Scrivito.provideWidgetClass('iubendaPrivacyWidget', {
+    attributes: {
+      privacy: 'string',
+      cookie: 'string',
+    },
+});
+
+Scrivito.provideEditingConfig('iubendaPrivacyWidget', {
+    title: 'iubenda Privacy Policy Widget',
+    description: 'Displays iubenda Privacy policy',
+  
+    attributes: {
+      privacy: {
+        title: 'Privacy Policy',
+        description: 'Insert the url to the privacy policy. (non-markup version recommended)',
+      },
+  
+      cookie: {
+        title: 'Cookie Policy',
+        description: 'Insert the url to the cookie policy. (non-markup version recommended)',
+      },
+    },
+  
+    properties: ['privacy', 'cookie'],
+});
+
+class iubendaPrivacyWidget extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { privacyresponse: '', cookieresponse: '' };
+    }
+
+    componentDidMount() {
+        const widget = this.props.widget;
+        if(widget.get("privacy") != "" && widget.get("privacy").includes("iubenda.com"))
+        {
+            axios.get(widget.get("privacy"))
+            .then(response => {
+                this.setState({ privacyresponse: response.data.content });
+            })
+            .catch(error => {
+                console.error(error);
+                this.setState({ privacyresponse: error })
+            });
+        }
+        else    {
+            this.setState({ privacyresponse: 'Please insert the API url' })
+        }
+
+        if(widget.get("cookie") != "" && widget.get("cookie").includes("iubenda.com"))
+        {
+            axios.get(widget.get("cookie"))
+            .then(response => {
+                this.setState({ cookieresponse: response.data.content });
+            })
+            .catch(error => {
+                console.error(error);
+                this.setState({ cookieresponse: error })
+            });
+        }
+        else    {
+            this.setState({ cookieresponse: 'Please insert the API url' })
+        }
+    }
+
+    render() {
+        return (
+        <div>
+            <div id="iubenda-privacy"><div dangerouslySetInnerHTML={{__html: this.state.privacyresponse}} /></div>
+        <hr />
+            <div id="iubenda-cookie"><div dangerouslySetInnerHTML={{__html: this.state.cookieresponse}} /></div>
+        </div>
+        )
+    }
+}
+
+Scrivito.provideComponent('iubendaPrivacyWidget', iubendaPrivacyWidget);
